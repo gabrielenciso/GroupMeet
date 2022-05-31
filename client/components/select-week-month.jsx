35ace4ac4e113ee1e8b5326ff3268d31023770ba
gabrielenciso <itsgabriel.enciso@gmail.com>
2022-05-31 22:28:24 +0000

    added event handler and listener and logged to console

--- client/components/select-week-month.jsx
+++ client/components/select-week-month.jsx
@@ -2,47 +2,54 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 
-function createWeek() {
+function CreateWeek(props) {
   const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   const week = days.map((day, index) =>
-    <span key={index} className='h-8 w-8 mx-5'>
-      {day}
-    </span>
-    )
+    <span key={index} onClick={props.onClick} data-selected={false}
 
-  return (
-    {week}
-  )
+      className='h-full w-12 bg-gray-300 font-nunito-sans font-thin center-all
+                                  lg:w-16'>
+      {day}
+    </span>)
+
+  return week
 }
 
 class SelectWeekMonth extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      week: true
-
+      week: true,
+      daysSelected: []
     }
 
     this.handleSelectChange = this.handleSelectChange.bind(this);
+    this.handleSelectWeekDays = this.handleSelectWeekDays.bind(this);
   }
 
   handleSelectChange(event) {
     console.log(event.target.value)
   }
 
-  render() {
-    const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-    const week = days.map((day, index) =>
-      <span key={index} className='h-full w-12 bg-gray-300 font-nunito-sans font-thin center-all
-                                  lg:w-16'>
-        {day}
-      </span>)
+  handleSelectWeekDays(event) {
+    console.log(event.target)
+  }
 
+  render() {
+    // const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    // const week = days.map((day, index) =>
+    //   <span key={index} onClick={this.handleSelectWeekDays}
+
+    //   className='h-full w-12 bg-gray-300 font-nunito-sans font-thin center-all
+    //                               lg:w-16'>
+    //     {day}
+    //   </span>)
+    const { handleSelectChange, handleSelectWeekDays } = this;
     return (
       <div className='w-full mx-5 min-h-fit
                       lg:w-116 lg:mt-10 lg:order-4 lg:mx-0'>
           <div className='w-full text-center mt-8 mb-5'>
-            <select onChange={this.handleSelectChange}
+            <select onChange={handleSelectChange}
 
               className='select select-ghost w-3/4 p-1 font-nunito-sans font-light text-xl active:outline-none
       focus:outline-none focus:border-b focus:border-b-blue-500
@@ -55,7 +62,7 @@ class SelectWeekMonth extends React.Component {
 
           <div className='w-full h-12 flex justify-between
                           lg:h-16'>
-            {week}
+            <CreateWeek onClick={handleSelectWeekDays} />
           </div>
       </div>
     )

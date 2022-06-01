8f962b33669fd3ad1e525bae09805655a9cb993a
gabrielenciso <itsgabriel.enciso@gmail.com>
2022-06-01 05:59:36 +0000

    added toggle funcionality and also click and drag to select, running into a bug

--- client/components/select-week-month.jsx
+++ client/components/select-week-month.jsx
@@ -3,16 +3,22 @@ import ReactDOM from 'react-dom/client'
 
 
 function CreateWeek(props) {
+  const {mousedown, mouseup, selecting, selectdays} = props;
   const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-  const week = days.map((day, index) =>
-    <span key={index} onClick={props.onClick} data-selected={false}
+  const week = days.map((day, index) => {
+    const color = selectdays.includes(day) ? 'bg-green-500' : 'bg-gray-300';
 
-      className='h-full w-12 bg-gray-300 font-nunito-sans font-thin center-all
-                                  lg:w-16'>
-      {day}
-    </span>)
+    return(
+      <span key={index} onMouseDown={mousedown} onMouseUp={mouseup} onMouseOver={selecting} value={day}
 
-  return week
+        className={`h-full w-12 ${color} font-nunito-sans font-thin center-all hover:cursor-pointer
+                                  lg:w-16`}>
+        {day}
+      </span>
+    )
+  });
+
+  return week;
 }
 
 class SelectWeekMonth extends React.Component {
@@ -20,31 +26,82 @@ class SelectWeekMonth extends React.Component {
     super(props);
     this.state = {
       week: true,
-      daysSelected: []
+      daysSelected: [],
+      toggle: false,
+      selecting: false
     }
 
     this.handleSelectChange = this.handleSelectChange.bind(this);
     this.handleSelectWeekDays = this.handleSelectWeekDays.bind(this);
+    this.handleMouseDown = this.handleMouseDown.bind(this);
+    this.handleMouseUp = this.handleMouseUp.bind(this);
+    this.handleMouseOut = this.handleMouseOut.bind(this);
   }
 
   handleSelectChange(event) {
     console.log(event.target.value)
   }
 
+  changeDaysSelected(event) {
+    const selectedVal = event.target.getAttribute('value');
+    const days = this.state.daysSelected;
+
+    if (!days.includes(selectedVal)) {
+      this.setState({ daysSelected: [...this.state.daysSelected, selectedVal],
+                       selecting: true });
+    } else {
+      days.splice(days.indexOf(selectedVal), 1);
+      this.setState({ daysSelected: days,
+                      selecting: false });
+    }
+  }
+  //hover
   handleSelectWeekDays(event) {
-    console.log(event.target)
+    if (!this.state.toggle) return
+
+    const selectedVal = event.target.getAttribute('value');
+    const days = this.state.daysSelected.slice();
+
+    if (this.state.selecting) {
+      this.setState({ daysSelected: [...this.state.daysSelected, selectedVal]})
+    } else {
+      days.splice(days.indexOf(selectedVal), 1);
+      this.setState({ daysSelected: days});
+    }
+
+  }
+
+  handleMouseDown(event) {
+    this.setState({ toggle: true });
+
+    const selectedVal = event.target.getAttribute('value');
+    const days = this.state.daysSelected.slice();
+
+    if (!days.includes(selectedVal)) {
+      this.setState({
+        daysSelected: [...this.state.daysSelected, selectedVal],
+        selecting: true
+      });
+    } else {
+      days.splice(days.indexOf(selectedVal), 1);
+      this.setState({
+        daysSelected: days,
+        selecting: false
+      });
+    }
+    // this.changeDaysSelected(event);
+  }
+
+  handleMouseUp() {
+    this.setState({ toggle: false })
+  }
+
+  handleMouseOut() {
+    this.setState({ toggle: false });
   }
 
   render() {
-    // const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-    // const week = days.map((day, index) =>
-    //   <span key={index} onClick={this.handleSelectWeekDays}
-
-    //   className='h-full w-12 bg-gray-300 font-nunito-sans font-thin center-all
-    //                               lg:w-16'>
-    //     {day}
-    //   </span>)
-    const { handleSelectChange, handleSelectWeekDays } = this;
+    const { handleSelectChange, handleSelectWeekDays, handleMouseDown, handleMouseUp, handleMouseOut } = this;
     return (
       <div className='w-full mx-5 min-h-fit
                       lg:w-116 lg:mt-10 lg:order-4 lg:mx-0'>
@@ -60,9 +117,9 @@ class SelectWeekMonth extends React.Component {
             </select>
           </div>
 
-          <div className='w-full h-12 flex justify-between
+          <div onMouseLeave={handleMouseOut} className='w-full h-12 flex justify-between select-none
                           lg:h-16'>
-            <CreateWeek onClick={handleSelectWeekDays} />
+            <CreateWeek mousedown={handleMouseDown} mouseup={handleMouseUp} selecting={handleSelectWeekDays} selectdays={this.state.daysSelected} />
           </div>
       </div>
     )

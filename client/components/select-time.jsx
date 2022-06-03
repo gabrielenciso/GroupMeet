11746b10052de2f37a48a9d322775b92e8ccc5ef
gabrielenciso <itsgabriel.enciso@gmail.com>
2022-06-03 19:43:11 +0000

    added event listeners for select cmoponent

--- client/components/select-time.jsx
+++ client/components/select-time.jsx
@@ -6,16 +6,37 @@ export default class SelectTime extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-
+      hour: 9,
+      minute: 0,
+      ampm: 'am'
     }
+
+    this.handleHourChange =  this.handleHourChange.bind(this);
+    this.handleMinuteChange = this.handleMinuteChange.bind(this);
+    this.handleAmPmChange = this.handleAmPmChange.bind(this);
+  }
+
+  handleHourChange(event) {
+    console.log(event.target.value);
+    this.setState({ hour: event.target.value });
+  }
+
+  handleMinuteChange(event) {
+    console.log(event.target.value);
+    this.setState({ minute: event.target.value });
+  }
+
+  handleAmPmChange(event) {
+    console.log(event.target.value)
+    this.setState({ ampm: event.target.value });
   }
 
   render() {
+    const { handleHourChange, handleMinuteChange, handleAmPmChange } = this;
     return(
       <div className="py-1 px-3 w-32 font-nunito-sans bg-white rounded-lg border border-gray-400">
         <div className="flex justify-around">
-          <select name="hours"
-
+          <select name="hours" onChange={handleHourChange}
           className="bg-transparent text-xl appearance-none outline-none font-light">
             <option value="1">1</option>
             <option value="2">2</option>
@@ -31,11 +52,13 @@ export default class SelectTime extends React.Component {
             <option value="12">12</option>
           </select>
           <span className="text-xl mr-3">:</span>
-          <select name="minutes" className="bg-transparent text-xl appearance-none outline-none mr-4 font-light">
+          <select name="minutes" onChange={handleMinuteChange}
+          className="bg-transparent text-xl appearance-none outline-none mr-4 font-light">
             <option value="0">00</option>
             <option value="30">30</option>
           </select>
-          <select name="ampm" className="bg-transparent text-xl appearance-none outline-none font-light">
+          <select name="ampm" onChange={handleAmPmChange}
+          className="bg-transparent text-xl appearance-none outline-none font-light">
             <option value="am">AM</option>
             <option value="pm">PM</option>
           </select>

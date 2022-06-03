26983758b7445c3e25d1aabaca9ae0b61d620fff
gabrielenciso <itsgabriel.enciso@gmail.com>
2022-06-03 20:30:30 +0000

    able to send state up from select component and update choose time state

--- client/components/choose-time-range.jsx
+++ client/components/choose-time-range.jsx
@@ -16,23 +16,89 @@ export default class ChooseTimeRange extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      timeStart: null,
-      timeEnd: null
+      timeStart: {
+        hour: '1',
+        minute: '0',
+        ampm: 'AM'
+      },
+      timeEnd: {
+        hour: '1',
+        minute: '0',
+        ampm: 'AM'
+      }
+    }
+
+    this.handleStartTime = this.handleStartTime.bind(this);
+    this.handleEndTime = this.handleEndTime.bind(this);
+  }
+
+  handleStartTime(event) {
+    const { timeStart } = this.state;
+    const name = event.target.name;
+    if (name === 'hour') {
+      this.setState(prevState => ({
+        timeStart: {
+          ...prevState.timeStart,
+          hour: event.target.value
+        }
+      }));
+    } else if (name === 'minute') {
+      this.setState(prevState => ({
+        timeStart: {
+          ...prevState.timeStart,
+          minute: event.target.value
+        }
+      }));
+    } else if (name === 'ampm') {
+      this.setState(prevState => ({
+        timeStart: {
+          ...prevState.timeStart,
+          ampm: event.target.value
+        }
+      }));
+    }
+  }
+
+
+  handleEndTime(event) {
+    const { timeEnd } = this.state;
+    const name = event.target.name;
+    if (name === 'hour') {
+      this.setState(prevState => ({
+        timeEnd: {
+          ...prevState.timeEnd,
+          hour: event.target.value
+        }
+      }));
+    } else if (name === 'minute') {
+      this.setState(prevState => ({
+        timeEnd: {
+          ...prevState.timeEnd,
+          minute: event.target.value
+        }
+      }));
+    } else if (name === 'ampm') {
+      this.setState(prevState => ({
+        timeEnd: {
+          ...prevState.timeEnd,
+          ampm: event.target.value
+        }
+      }));
     }
   }
 
   render() {
+    const { handleStartTime, handleEndTime } = this;
     return (
       <div className='w-full text-center mt-12
                       lg:w-96 lg:order-2'>
         <ChooseTimeLabel />
         <div className='w-4/5 flex justify-between items-center m-auto'>
-          <SelectTime />
+          <SelectTime getTime={handleStartTime}/>
           <span className='font-nunito-sans font-thin text-lg'>to</span>
-          <SelectTime />
+          <SelectTime getTime={handleEndTime} />
         </div>
       </div>
     )
   }
-
 }
--- client/components/select-time.jsx
+++ client/components/select-time.jsx
@@ -5,38 +5,39 @@ export default class SelectTime extends React.Component {
 
   constructor(props) {
     super(props)
-    this.state = {
-      hour: 9,
-      minute: 0,
-      ampm: 'am'
-    }
+    // this.state = {
+    //   hour: '1',
+    //   minute: '00',
+    //   ampm: 'am'
+    // }
 
-    this.handleHourChange =  this.handleHourChange.bind(this);
-    this.handleMinuteChange = this.handleMinuteChange.bind(this);
-    this.handleAmPmChange = this.handleAmPmChange.bind(this);
+    // this.handleHourChange =  this.handleHourChange.bind(this);
+    // this.handleMinuteChange = this.handleMinuteChange.bind(this);
+    // this.handleAmPmChange = this.handleAmPmChange.bind(this);
   }
 
-  handleHourChange(event) {
-    console.log(event.target.value);
-    this.setState({ hour: event.target.value });
-  }
+  // handleHourChange(event) {
+  //   console.log(event.target.name);
+  //   this.setState({ hour: event.target.value });
+  // }
 
-  handleMinuteChange(event) {
-    console.log(event.target.value);
-    this.setState({ minute: event.target.value });
-  }
+  // handleMinuteChange(event) {
+  //   console.log(event.target.value);
+  //   this.setState({ minute: event.target.value });
+  // }
 
-  handleAmPmChange(event) {
-    console.log(event.target.value)
-    this.setState({ ampm: event.target.value });
-  }
+  // handleAmPmChange(event) {
+  //   console.log(event.target.value)
+  //   this.setState({ ampm: event.target.value });
+  // }
 
   render() {
-    const { handleHourChange, handleMinuteChange, handleAmPmChange } = this;
+    // const { handleHourChange, handleMinuteChange, handleAmPmChange } = this;
+    const { getTime } = this.props;
     return(
       <div className="py-1 px-3 w-32 font-nunito-sans bg-white rounded-lg border border-gray-400">
         <div className="flex justify-around">
-          <select name="hours" onChange={handleHourChange}
+          <select name="hour" onChange={getTime}
           className="bg-transparent text-xl appearance-none outline-none font-light">
             <option value="1">1</option>
             <option value="2">2</option>
@@ -52,15 +53,15 @@ export default class SelectTime extends React.Component {
             <option value="12">12</option>
           </select>
           <span className="text-xl mr-3">:</span>
-          <select name="minutes" onChange={handleMinuteChange}
+          <select name="minute" onChange={getTime}
           className="bg-transparent text-xl appearance-none outline-none mr-4 font-light">
             <option value="0">00</option>
             <option value="30">30</option>
           </select>
-          <select name="ampm" onChange={handleAmPmChange}
+          <select name="ampm" onChange={getTime}
           className="bg-transparent text-xl appearance-none outline-none font-light">
-            <option value="am">AM</option>
-            <option value="pm">PM</option>
+            <option value="AM">AM</option>
+            <option value="PM">PM</option>
           </select>
         </div>
       </div>
